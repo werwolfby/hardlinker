e3c9e77a3bda6e2d6e71fb8e611d8fdb2fee8280
Alexander Puzynia <werwolf.by@gmail.com>
2016-11-22 00:08:52 +0300

    feat: add simple falcon application with static file serving

---
+++ hardlinker/rest/__init__.py
@@ -0,0 +1,156 @@
+"""
+init code for run falcon API
+"""
+import json
+import datetime
+import falcon
+from enum import Enum
+from itsdangerous import JSONWebSignatureSerializer, BadSignature
+
+
+class HardlinkerJSONEncoder(json.JSONEncoder):
+    """
+    can return datetime in ISO format and Enum as regular string
+    """
+
+    # pylint: disable=E0202
+    # more info https://github.com/PyCQA/pylint/issues/414
+    def default(self, o):
+        """default method"""
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+        if isinstance(o, Enum):
+            return str(o)
+        return super(HardlinkerJSONEncoder, self).default(o)
+
+
+class HardlinkerRequest(falcon.Request):
+    """
+    support for json in request
+    """
+    json = None
+
+
+class HardlinkerResponse(falcon.Response):
+    """
+    support for json in response
+    """
+    json = None
+
+
+# noinspection PyMethodMayBeStatic,PyMethodMayBeStatic,PyUnusedLocal
+class JSONTranslator(object):
+    """
+    falcon middleware to read json from request and write json into response
+    """
+
+    # pylint: disable=W0613
+    def process_resource(self, req, resp, resource, params):
+        """
+        set json property on request
+
+        :type req: MonitorrentRequest
+        :type resp: MonitorrentResponse
+        """
+        if req.content_length in (None, 0):
+            return
+
+        body = req.stream.read()
+        try:
+            req.json = json.loads(body.decode('utf-8'))
+
+        except (ValueError, UnicodeDecodeError):
+            raise falcon.HTTPBadRequest('Malformed JSON',
+                                        'Could not decode the request body. The '
+                                        'JSON was incorrect or not encoded as '
+                                        'UTF-8.')
+
+    # pylint: disable=W0613
+    def process_response(self, req, resp, resource):
+        """
+        set body from json property on response
+
+        :type req: MonitorrentRequest
+        :type resp: MonitorrentResponse
+        """
+        if resp.json is None:
+            return
+
+        resp.body = json.dumps(resp.json, cls=HardlinkerJSONEncoder, ensure_ascii=False)
+
+
+# noinspection PyMethodMayBeStatic,PyMethodMayBeStatic,PyUnusedLocal
+class AuthMiddleware(object):
+    """
+    falcon middleware for authenticate requests over JWT
+    """
+
+    cookie_name = 'jwt'
+    serializer = None
+    token = None
+    auth_enabled = None
+
+    # pylint: disable=W0613
+    def process_resource(self, req, resp, resource, params):
+        """
+        validate auth before request, if resource marked with no_auth decorator ignore auth
+        if requests hasn't valid JWT token respon 401 will be returned
+        """
+        if getattr(resource, '__no_auth__', False):
+            return
+
+        if not self.validate_auth(req):
+            raise falcon.HTTPUnauthorized('Authentication required', 'AuthCookie is not specified', None)
+
+    @classmethod
+    def validate_auth(cls, req):
+        """check if auth_enabled and JWT token from request is valid"""
+        auth_enabled = cls.auth_enabled
+        if auth_enabled is not None and not auth_enabled():
+            return True
+
+        jwt = req.cookies.get(cls.cookie_name, None)
+        if jwt is None:
+            return False
+        try:
+            value = cls.serializer.loads(jwt)
+            return value == cls.token
+        except BadSignature:
+            return False
+
+    @classmethod
+    def authenticate(cls, resp):
+        """generate JWT token and write it to response"""
+        value = cls.serializer.dumps(cls.token).decode()
+        resp.set_cookie(cls.cookie_name, value, path='/', secure=False)
+
+    @classmethod
+    def logout(cls, resp):
+        """expire JWT token cookie"""
+        resp.set_cookie(cls.cookie_name, "", path='/', secure=False,
+                        expires=datetime.datetime.utcfromtimestamp(0))
+
+    @classmethod
+    def init(cls, secret_key, token, auth_enabled):
+        """init middleware"""
+        cls.serializer = JSONWebSignatureSerializer(secret_key)
+        cls.token = token
+        if auth_enabled is not None:
+            cls.auth_enabled = classmethod(lambda lcls: auth_enabled())
+        else:
+            cls.auth_enabled = None
+
+
+def no_auth(obj):
+    """decorator for disable resource authentication"""
+    obj.__no_auth__ = True
+    return obj
+
+
+def create_api(disable_auth=False):
+    """create falcon API with Json and Auth middlewares"""
+    middleware = list()
+    middleware.append(JSONTranslator())
+    if not disable_auth:
+        middleware.append(AuthMiddleware())
+    return falcon.API(request_type=HardlinkerRequest, response_type=HardlinkerResponse, middleware=middleware)
---
+++ server.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+from builtins import range
+import os
+import sys
+import six
+import random
+import string
+import argparse
+import warnings
+from cherrypy import wsgiserver
+from hardlinker.rest import create_api, AuthMiddleware
+from hardlinker.rest.static_file import StaticFiles
+
+
+def add_static_route(api, files_dir):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    static_dir = os.path.join(file_dir, files_dir)
+    api.add_route('/', StaticFiles(static_dir, 'index.html', False))
+    #api.add_route('/styles/monitorrent.css', StaticFiles(os.path.join(static_dir, 'styles'), 'monitorrent.css', False))
+    for d, dirnames, files in os.walk(static_dir):
+        parts = d[len(file_dir)+1:].split(os.path.sep)
+        url = '/' + '/'.join(parts[1:] + ['{filename}'])
+        api.add_route(url, StaticFiles(d, redirect_to_login=False))
+
+
+def create_app(secret_key, token):
+    AuthMiddleware.init(secret_key, token, None)
+    app = create_api()
+    add_static_route(app, 'webapp')
+    return app
+
+
+def main():
+    def try_int(s, base=10, val=None):
+        if s is None:
+            return None
+        try:
+            return int(s, base)
+        except ValueError:
+            return val
+
+    class Config(object):
+        debug = False
+        ip = '0.0.0.0'
+        port = 4735                      # HRDL on phone keyboard
+        config = 'config.py'
+
+        def __init__(self, parsed_args):
+            if parsed_args.config is not None and not os.path.isfile(parsed_args.config):
+                warnings.warn('File not found: {}'.format(parsed_args.config))
+            config_path = parsed_args.config or self.config
+            if os.path.isfile(config_path):
+                # noinspection PyBroadException
+                try:
+                    parsed_config = {}
+                    with open(config_path) as config_file:
+                        six.exec_(compile(config_file.read(), config_path, 'exec'), {}, parsed_config)
+                    self.debug = parsed_config.get('debug', self.debug)
+                    self.ip = parsed_config.get('ip', self.ip)
+                    self.port = parsed_config.get('port', self.port)
+                    self.db_path = parsed_config.get('db_path', self.db_path)
+                except:
+                    ex, val, tb = sys.exc_info()
+                    warnings.warn('Error reading: {0}: {1} ({2}'.format(parsed_args.config, ex, val))
+
+            env_debug = (os.environ.get('MONITORRENT_DEBUG', None) in ['true', 'True', '1'])
+
+            self.debug = parsed_args.debug or env_debug or self.debug
+            self.ip = parsed_args.ip or os.environ.get('MONITORRENT_IP', None) or self.ip
+            self.port = parsed_args.port or try_int(os.environ.get('MONITORRENT_PORT', None)) or self.port
+
+    parser = argparse.ArgumentParser(description='Hardlinker server')
+    parser.add_argument('--debug', action='store_true',
+                        help='Run in debug mode. Secret key is always the same.')
+    parser.add_argument('--ip', type=str, dest='ip',
+                        help='Bind interface. Default is {0}'.format(Config.ip))
+    parser.add_argument('--port', type=int, dest='port',
+                        help='Port for server. Default is {0}'.format(Config.port))
+    parser.add_argument('--config', type=str, dest='config',
+                        default=os.environ.get('MONITORRENT_CONFIG', None),
+                        help='Path to config file (default {0})'.format(Config.config))
+
+    parsed_args = parser.parse_args()
+    config = Config(parsed_args)
+
+    debug = config.debug
+
+    if debug:
+        secret_key = 'Secret!'
+        token = 'monitorrent'
+    else:
+        secret_key = os.urandom(24)
+        token = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+
+    app = create_app(secret_key, token)
+    d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
+    server_start_params = (config.ip, config.port)
+    server = wsgiserver.CherryPyWSGIServer(server_start_params, d)
+    print('Server started on {0}:{1}'.format(*server_start_params))
+
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+
+
+if __name__ == '__main__':
+    main()
