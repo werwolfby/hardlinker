3a3b69c73f52db96e382d6058e137ec63018c26b
Alexander Puzynia <werwolf.by@gmail.com>
2016-12-01 00:02:10 +0300

    feat: remove show absolute path check box

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -13,11 +13,6 @@ import { GuessItService }  from "./guessit.service";
                     <label><input name="showWithoutLinksOnlyCheckbox" type="checkbox" [ngModel]="showWithoutLinksOnly" (ngModelChanged)="showWithoutLinksOnly = $event">Show without links only</label>
                 </div>
             </div>
-            <div class="pull-right">
-                <div class="checkbox">
-                    <label><input name="showAbsolutePathCheckbox" type="checkbox"  [ngModel]="showAbsolutePath" (ngModelChanged)="showAbsolutePath = $event">Show absolute path</label>
-                </div>
-            </div>
         </div>
         <hl-links-table></hl-links-table>
     </form>
@@ -26,7 +21,6 @@ import { GuessItService }  from "./guessit.service";
 })
 export class BrowseComponent {
     showWithoutLinksOnly = true;
-    showAbsolutePath = false;
 
     constructor() {
     }
